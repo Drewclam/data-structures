a336085748ff9a20ee5e0d52f72cc78f4cc912b3
Drewclam <andrew.c.lam94@gmail.com>
2017-03-30 10:35:16 -0700

    Fix all tests

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,29 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.front = 0;
+  this.back = 0;
 };
+
+Queue.prototype.enqueue = function(value) {
+  this.storage[this.front] = value;
+  this.front++;
+};
+
+Queue.prototype.dequeue = function() {
+  var removed = this.storage[this.back];
+  delete this.storage[this.back];
+  this.back++;
+  return removed;
+};
+
+Queue.prototype.size = function() {
+  return Object.keys(this.storage).length;
+};
+
+var ourQueue = new Queue();
+
+
 
 
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,26 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.top = 0;
+  this.storage = {};
 };
 
+Stack.prototype.push = function(value) {
+  this.storage[this.top] = value;
+  this.top++;
+};
 
+Stack.prototype.pop = function() {
+  if (this.top) {
+    this.top--;
+    var popped = this.storage[this.top];
+    delete this.storage[this.top];
+    return popped;
+  }
+};
+
+Stack.prototype.size = function() {
+  return Object.keys(this.storage  ).length;
+};
+
+var ourStack = new Stack();
