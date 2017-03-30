2304c28bc2d5ac9ec889e928967c2eb50805a26b
Drewclam <andrew.c.lam94@gmail.com>
2017-03-30 10:11:50 -0700

    Fix prototypal stack and queue

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,28 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(queueMethods);
+  someInstance.storage = {};
+  someInstance.front = 0;
+  someInstance.back = 0;
+  return someInstance;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value) {
+  this.storage[this.front] = value;
+  this.front++;
+};
+
+queueMethods.dequeue = function() {
+  var popped = this.storage[this.back];
+  delete this.storage[this.back];
+  this.back++;
+  return popped;
+};
+
+queueMethods.size = function() {
+  return Object.keys(this.storage).length;
+};
 
