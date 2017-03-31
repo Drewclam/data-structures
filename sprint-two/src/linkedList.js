2e250fbaf8b350848252e6d12e55df1ead9bdace
Drewclam <andrew.c.lam94@gmail.com>
2017-03-30 21:07:52 -0700

    Fix tree.js

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,26 +4,50 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var node = new Node(value);
+    if (this.head === null && this.tail === null) { // if linked list is empty
+      this.head = node;
+      this.head.next = this.tail;
+      this.tail = node;
+    } else {
+      this.tail.next = node;
+      this.tail = node;
+    }
   };
 
   list.removeHead = function() {
+    var popped = this.head.value;
+    this.head = this.head.next;
+    return popped;
   };
 
   list.contains = function(target) {
+    var node = this.head;
+    if (node.value === target) {
+      return true;
+    }
+    while (node.next) {
+      if (node.next.value === target) {
+        return true;
+      }
+      node = node.next;
+    }
+    return false;
   };
-
   return list;
 };
 
 var Node = function(value) {
   var node = {};
-
   node.value = value;
   node.next = null;
-
   return node;
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+ // addToTail and removeHead is constant time complexity
+ // contains is linear time complexity
+
+
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,9 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
-
+  _.extend(newTree, treeMethods);
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
 
   return newTree;
 };
@@ -11,9 +11,21 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var newTree = new Tree(value);
+  this.children.push(newTree);
 };
 
 treeMethods.contains = function(target) {
+  if (this.value === target) {
+    return true;
+  }
+  for (var i = 0; i < this.children.length; i++) {
+    var currentChild = this.children[i];
+    if (currentChild.contains(target)) {
+      return true;
+    }
+  }
+  return false;
 };
 
 
@@ -21,3 +33,5 @@ treeMethods.contains = function(target) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+ // addChild() is linear time complexity
+ // contains() is linear time complexity
