3a562b473c322ee4700a868340d12325e102781f
Drewclam <andrew.c.lam94@gmail.com>
2017-04-01 11:30:16 -0700

    hashTable.js pass all specs except dynamically changinge length
    Implemented buckets

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -12,15 +12,25 @@ HashTable.prototype.insert = function(k, v) { // create and inserts tuple
   // if this._storage.get(index) is undefined
   if (this._storage.get(index) === undefined) {
     // then create bucket at index
-    var bucket = LimitedArray(2);
+    var bucket = [];
+    bucket.push(tuple);
     this._storage.set(index, bucket);
-    bucket.set(0, tuple);
   }
-  var bucket = this._storage.get(index);
-  if (bucket.get(0) === undefined) {
-    bucket.set(0, tuple);
-  } else {
-    bucket.set(1, tuple);
+  // search bucket
+
+  var bucket = this._storage.get(index); //
+  var isFound = false;
+  for (var i = 0; i < bucket.length; i++) {
+    // if any tuple[0] is k
+    if (bucket[i][0] === k) {
+      // make that tuple[1] = v
+      bucket[i][1] = v;
+      isFound = true;
+    }
+    // else push tuple to bucket
+  }
+  if (!isFound) {
+    bucket.push(tuple);
   }
 
       // else if bucket already exists at index,
@@ -35,15 +45,13 @@ HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   // get bucket at index
   var retrievedBucket = this._storage.get(index); // [ ['Andrew', 'Ilke'], ['robin', 'rebecca'] ];
-  console.log(retrievedBucket);
   // search bucket index 0
-  if (retrievedBucket && retrievedBucket[0][0] === k) {
-    return retrievedBucket[0][1];
-    // if k is at index 0 of a bucket item
-      // return index 1 of that bucket item
-  } else if (retrievedBucket && retrievedBucket[1][0] === k) {
-    return retrievedBucket[1][1];
-
+  if (retrievedBucket !== undefined) {
+    for (var i = 0; i < retrievedBucket.length; i++) {
+      if (retrievedBucket[i][0] === k) {
+        return retrievedBucket[i][1];
+      }
+    }
   }
   // searh bucket index 1
     // if k is at index 0 of a bucket item
@@ -62,5 +70,4 @@ var ourHashTable = new HashTable();
 /*
  * Complexity: What is the time complexity of the above functions?
  */
-
 
